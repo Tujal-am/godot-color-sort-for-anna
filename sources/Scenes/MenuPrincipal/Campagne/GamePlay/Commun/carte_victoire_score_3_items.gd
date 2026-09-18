778e910ce4e8033d41bd97fe045b83455fe3669a
leguims <leguims@free.fr>
2026-09-18 16:19:01 +0200

    Creation de cartes pour peupler le score des gameplays

---
+++ sources/Scenes/MenuPrincipal/Campagne/GamePlay/Commun/carte_victoire_score_3_items.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@export var _titre: String = "Titre Score":
+	set(texte):
+		_titre = texte
+		# Si le nœud est prêt dans l'arbre, on met à jour le Label
+		if is_inside_tree():
+			$Titre.text = _titre
+
+@export_file("victoire_*.png") var _chemin_logo: String = "res://Art/UI/Gameplay/Commun/victoire_temps.png":
+	set(chemin):
+		_chemin_logo = chemin
+		# Si le nœud est prêt dans l'arbre, on met à jour le Label
+		if is_inside_tree():
+			$MarginContainer/Logo.texture = load(_chemin_logo)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	titre(_titre)
+	logo(_chemin_logo)
+
+func logo(chemin_logo : String) -> void:
+	_chemin_logo = chemin_logo
+	$MarginContainer/Logo.texture = load(chemin_logo)
+
+func titre(texte : String) -> void:
+	_titre = texte
+
+func item1(texte : String) -> void:
+	$ListeItems/Item1/Texte.text = texte
+
+func item2(texte : String) -> void:
+	$ListeItems/Item2/Texte.text = texte
+
+func item3(texte : String) -> void:
+	$ListeItems/Item3/Texte.text = texte
---
+++ sources/Scenes/MenuPrincipal/Campagne/GamePlay/Commun/carte_victoire_score_points.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func titre(texte : String) -> void:
+	$Titre.text = texte
+
+func points(texte : String) -> void:
+	$Points.text = texte
