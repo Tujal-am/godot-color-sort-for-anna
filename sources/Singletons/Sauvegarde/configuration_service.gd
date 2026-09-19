da5a74a7fb73ab30a6b311c00e889bbaba45ea83
leguims <leguims@free.fr>
2026-09-19 15:08:22 +0200

    Prochaine version

--- sources/Singletons/Sauvegarde/configuration_service.gd
+++ sources/Singletons/Sauvegarde/configuration_service.gd
@@ -8,7 +8,7 @@ extends Node
 
 # Dico : {'caracteristique': reglage}
 var configuration_du_jeu = {
-	'version': 'V1.0.0-rc8',
+	'version': 'V1.0.0-rc9',
 	'date_debut_campagne': "2026-09-01 00:00:00",
 	'musiques': true,
 	'effets sonores': true,
